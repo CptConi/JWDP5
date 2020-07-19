ed31a0e5880d359902e32c77882a386c35990b25
CptConi <nicolas.renard.33@gmail.com>
2020-07-19 17:34:17 +0200

    refaco queries.js add commandBtn add new window open w/ id on session storage

---
+++ item-details.js
@@ -0,0 +1,2 @@
+console.log("Vous êtes sur la page d'informations pour le super teddy bear !");
+console.log(sessionStorage.getItem('teddyId'));
--- queries.js
+++ queries.js
@@ -1,51 +1,92 @@
+// const e = require("express");
+
 let teddiesURL = "http://localhost:3000/api/teddies";
 let result;
 let request = new XMLHttpRequest();
 
-// Requesting Teddies array
-request.onreadystatechange = function () {
-  if (request.readyState === 4) {
-    if (request.status == 200) {
+class Teddy {
+  constructor (pId, pName, pDescription, pPrice, pImageUrl){
+    this.id = pId;
+    this.name= pName;
+    this.description = pDescription;
+    this.price = pPrice;
+    this.imageUrl = pImageUrl;
+    this.color = null;
+  }
+
+  setColor(pColor){
+    this.color = pColor;
+  }
+  
+}
+
+// 
+request.addEventListener('load', function () {
+    if (request.status <= 200 && request.status <300) {
       //Inscrire ici les path pour accéder aux infos de la réponse server, avec un JSON.parce(request.responseText.[...])
       result = JSON.parse(request.responseText);
       // Affichage et formatage des différents éléments récupérés dans result
-      for (var response of result) {
-        cardSection = document.querySelector("#cards");
-        col = document.createElement("div");
-        div1 = document.createElement("div");
-        div2 = document.createElement("div");
-        img = document.createElement("img");
-        title = document.createElement("h2");
-        description = document.createElement("p");
-        price = document.createElement("p");
-
-        cardSection.appendChild(col);
-        col.appendChild(div1);
-        div1.appendChild(div2);
-        div1.appendChild(img);
-        div2.appendChild(title);
-        div1.appendChild(description);
-        div1.appendChild(price);
-
-        col.setAttribute("class", "col-4 my-3");
-        div1.setAttribute("class", "card shadow");
-        div2.setAttribute("class", "card-body");
-        img.setAttribute("class", "card-img-top h-75");
-        let imgPath = response.imageUrl;
-        img.setAttribute("src", imgPath);
-        title.setAttribute("class", "card-title text-center");
-        description.setAttribute("class", "card-text m-1");
-        price.setAttribute("class", "card-text text-right mr-3 mb-1 h4");
-
-        title.textContent = response.name;
-        description.textContent = response.description;
-        price.textContent = response.price + " €";
-      }
-    } else {
-      alert("Impossible de contacter le serveur distant.");
-    }
+      formatReqResult(result);
   }
-};
+})
 
 request.open("GET", teddiesURL, true);
 request.send();
+
+function formatReqResult(pPesult) {
+  for (var response of pPesult) {
+    const teddy = new Teddy(
+      response._id,
+      response.name,
+      response.description,
+      response.price,
+      response.imageUrl
+    );
+
+    cardSection = document.querySelector("#cards");
+    col = document.createElement("div");
+    div1 = document.createElement("div");
+    div2 = document.createElement("div");
+    img = document.createElement("img");
+    title = document.createElement("h2");
+    description = document.createElement("p");
+    price = document.createElement("p");
+    commandBtn = document.createElement("button");
+
+    cardSection.appendChild(col);
+    col.appendChild(div1);
+    div1.appendChild(div2);
+    div1.appendChild(img);
+    div2.appendChild(title);
+    div1.appendChild(description);
+    div1.appendChild(price);
+    div1.appendChild(commandBtn);
+
+    col.setAttribute("class", "col-4 my-3");
+    div1.setAttribute("class", "card shadow");
+    div2.setAttribute("class", "card-body");
+    img.setAttribute("class", "card-img-top h-75");
+    let imgPath = teddy.imageUrl;
+    img.setAttribute("src", imgPath);
+    title.setAttribute("class", "card-title text-center");
+    description.setAttribute("class", "card-text m-1");
+    price.setAttribute("class", "card-text text-right mr-3 my-2 h4");
+    commandBtn.id = 'commandeBtn';
+    commandBtn.setAttribute(
+      "class",
+      "btn btn-primary " + /*stretched-link*/ " w-50 mx-auto my-3"
+    );
+
+    commandBtn.addEventListener('click', function(){
+      sessionStorage.setItem('teddyId', teddy.id);
+      window.location = "item-details.html";
+    })
+
+    title.textContent = teddy.name;
+    description.textContent = teddy.description;
+    price.textContent = teddy.price + " €";
+    commandBtn.textContent = "Je le veux !";
+  }
+}
+
+// Pour stocker infos sur sesion storage > JSON.stringify puis JSON.parse
